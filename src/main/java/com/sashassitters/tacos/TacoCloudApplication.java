c47987613409ae26cabc86b098bbee1dc3c45ee8
Nick H <nhelmstetter@outlook.com>
2023-11-24 16:13:08 -0500

    Basic web app with single page

--- src/main/java/com/sashassitters/tacos/TacoCloudApplication.java
+++ src/main/java/com/sashassitters/tacos/TacoCloudApplication.java
@@ -1,4 +1,4 @@
-package com.sashassitters.tacocloud;
+package com.sashassitters.tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sashassitters/tacos/TacoCloudApplicationTests.java
+++ src/test/java/com/sashassitters/tacos/TacoCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sashassitters.tacocloud;
+package com.sashassitters.tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
