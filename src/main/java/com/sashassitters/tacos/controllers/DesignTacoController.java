3e611de6b5267a53a5307584e02d333a9cd6ea45
Nick H <nhelmstetter@outlook.com>
2023-11-26 15:04:58 -0500

    Added form validation

--- src/main/java/com/sashassitters/tacos/controllers/DesignTacoController.java
+++ src/main/java/com/sashassitters/tacos/controllers/DesignTacoController.java
@@ -4,9 +4,11 @@ import com.sashassitters.tacos.model.Ingredient;
 import com.sashassitters.tacos.model.Ingredient.Type;
 import com.sashassitters.tacos.model.Taco;
 import com.sashassitters.tacos.model.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -56,7 +58,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
--- src/main/java/com/sashassitters/tacos/model/Taco.java
+++ src/main/java/com/sashassitters/tacos/model/Taco.java
@@ -1,5 +1,8 @@
 package com.sashassitters.tacos.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -7,6 +10,10 @@ import java.util.List;
 @Data
 public class Taco {
 
+    @NotBlank
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
